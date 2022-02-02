1798e4e5b0acd922cc214863f5ace48f27ec6e54
Tamal Das <gyansujan69@gmail.com>
2022-02-02 13:00:18 +0530

    layout updated

--- milan-app/src/App.jsx
+++ milan-app/src/App.jsx
@@ -22,13 +22,12 @@ const App = () => {
                     {/* //* Auth routes - CLUBS*/}
 
                     {/* <Route exact path='/user/register' element={<ClubLogin />} />
+                    
                     <Route exact path='/user/login' element={<ClubRegister />} /> */}
 
                     {/* //* Display Routes */}
-                    {/* <Route exact path='/' element={<Home />} />
-                    <Route exact path='/' element={<Home />} />
-                    <Route exact path='/' element={<Home />} />
-                    <Route exact path='/' element={<Home />} /> */}
+                    {/* <Route exact path='/display/clubs' element={<ClubsPage />} /> */}
+
 
                     {/* //* Donations */}
                     {/* <Route exact path='/' element={<Home />} />
---
+++ milan-app/src/pages/display/ClubsPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ClubsPage = () => {
+    return <div></div>;
+};
+
+export default ClubsPage;
