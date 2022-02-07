9ac5fa8d632b989da2b158048b224d9f0998283e
soumali28 <gorai.soumali@gmail.com>
2022-02-07 10:17:15 +0530

    Adding the footer component

--- milan-app/src/pages/Home.jsx
+++ milan-app/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../components/Banner';
 import HomeCardsContainer from '../components/HomeCardsContainer';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const Home = () => {
     return (<>
         <Navbar />
         <Banner />
         <HomeCardsContainer />
+        <Footer />
     </>);
 };
 
