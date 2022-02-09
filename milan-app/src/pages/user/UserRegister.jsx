340982609f734f00c3c050769bf100641ca27e61
Tamal Das <gyansujan69@gmail.com>
2022-02-09 19:35:26 +0530

    Updated Styles

--- milan-app/src/pages/user/UserRegister.jsx
+++ milan-app/src/pages/user/UserRegister.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
-import Navbar from "../../components/Navbar";
-import registerImg from "../../assets/pictures/register.jpg";
-import "../../styles/UserRegister.css";
+import { FcHome } from "react-icons/fc";
 import { Link } from "react-router-dom";
+import Navbar from "../../components/Navbar";
 
-export default function UserRegister() {
+//* The styles for Login and Register are essentially same
+import "../../styles/UserLogin.css"
+
+
+function UserRegister() {
+	function UserImage() {
+		return (
+			<div className="col-md-8 col-lg-7 col-xl-6">
+				<img
+					className="img-fluid"
+					alt="Phone"
+					style={{ width: "auto" }}
+					src="https://www.getillustrations.com/packs/plastic-illustrations-scene-builder-pack/scenes/_1x/accounts%20_%20man,%20workspace,%20desk,%20laptop,%20login,%20user_md.png"
+				></img>
+			</div>
+		);
+	}
 
 	function Anchor(props) {
 		return (
@@ -18,140 +33,137 @@ export default function UserRegister() {
 	}
 
 	const [credentials, setCredentials] = useState({
-		firstName: "",
-		lastName: "",
 		email: "",
 		password: "",
-		address: "",
-		pin: "",
 	});
 
-	const handleOnChange = (event) => {
-		setCredentials({
-			...credentials,
-			[event.target.name]: event.target.value,
-		});
+	const [isEmailValid, setIsEmailValid] = useState(false);
+
+	const handleChange = (e) => {
+		setCredentials({ ...credentials, [e.target.name]: e.target.value });
+		if (
+			e.target.name === "email" &&
+			e.target.value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+		)
+			setIsEmailValid(true);
 	};
 
-	const handleSubmit = (event) => {
-		event.preventDefault();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
 		console.log(credentials);
 		console.log("Form submitted");
 	};
 
 	return (
-		<div id="userRegister">
+		<>
 			<Navbar />
-			<div className="container py-5 h-100">
-				<div className="row d-flex align-items-center justify-content-center h-100">
-					{/* Image column */}
-					<div id="img-col" className="col-md-8 col-lg-7 col-xl-6">
-						<img
-							className="img-fluid"
-							src="https://www.getillustrations.com/packs/plastic-illustrations-scene-builder-pack/scenes/_1x/accounts%20_%20man,%20workspace,%20desk,%20laptop,%20login,%20user_md.png"
-							alt="Phone"
-						/>
-					</div>
 
-					{/* Form column */}
-					<div id="form-col" className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-						<h2>Join Us Now!</h2>
-						<form onSubmit={handleSubmit}>
-							{/* <div id="name"> */}
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="firstName">First Name</label>
-								<input
-									type="text"
-									className="form-control"
-									id="firstName"
-									name="firstName"
-									value={credentials.firstName}
-									onChange={handleOnChange}
-									placeholder="First name"
-									required
-								/>
-							</div>
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="lastName">Last Name</label>
-								<input
-									type="text"
-									className="form-control"
-									id="lastName"
-									name="lastName"
-									value={credentials.lastName}
-									onChange={handleOnChange}
-									placeholder="Last name"
-									required
-								/>
-							</div>
-							{/* </div> */}
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="email">Email address</label>
-								<input
-									type="email"
-									className="form-control"
-									id="email"
-									name="email"
-									value={credentials.email}
-									onChange={handleOnChange}
-									placeholder="name@example.com"
-									required
-								/>
-							</div>
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="password">Password</label>
-								<input
-									type="password"
-									className="form-control"
-									id="password"
-									name="password"
-									value={credentials.password}
-									onChange={handleOnChange}
-									placeholder="Password"
-									required
-								/>
-							</div>
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="address">Address</label>
-								<textarea
-									className="form-control"
-									id="address"
-									name="address"
-									value={credentials.address}
-									onChange={handleOnChange}
-									rows="2"
-									placeholder="Address"
-									required
-								></textarea>
-							</div>
-							<div className="form-outline mb-2 form-group">
-								<label className="col-form-label col-form-label-md" htmlFor="pin">PIN Code</label>
-								<input
-									type="text"
-									className="form-control"
-									id="pin"
-									name="pin"
-									value={credentials.pin}
-									onChange={handleOnChange}
-									placeholder="Pincode"
-									required
-								/>
-							</div>
-							<div id="links">
-								<button className="btn btn-lg btn-block" type="submit">
-									Sign Up
+			<section class="vh-100">
+				<div class="container py-5 h-100">
+					<div class="row d-flex align-items-center justify-content-center h-100">
+						<div class="col-md-8 col-lg-7 col-xl-6">
+							<img src="https://www.getillustrations.com/packs/plastic-illustrations-scene-builder-pack/scenes/_1x/accounts%20_%20man,%20workspace,%20desk,%20laptop,%20login,%20user_md.png" class="img-fluid" alt="Phone" />
+						</div>
+
+						<div class="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+							<form style={{ width: "auto" }} onSubmit={handleSubmit}>
+
+								<h2 style={{ letterSpacing: "1px" }}>Join us at Milan !!</h2>
+
+								{/* //* First name ,last name */}
+								<div class="form-outline mb-4">
+
+									<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="First name" />
+
+								</div>
+
+								<div class="form-outline mb-4">
+									<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Last name" />
+
+								</div>
+
+
+								{/* //* Email */}
+								<div class="form-outline mb-4">
+									<label for="exampleInputEmail1" class=" col-form-label col-form-label-lg">Email address</label>
+
+									<input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" name="email" value={credentials.email} onChange={handleChange} required />
+
+								</div>
+
+								{/* //* Password */}
+								<div class="form-outline mb-4">
+									<label for="exampleInputPassword1" class="col-form-label col-form-label-lg">Password</label>
+
+									<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" name="password" value={credentials.password} onChange={handleChange} required />
+
+								</div>
+
+								{/* //* Adress */}
+								<div class="form-outline mb-4">
+									<label for="exampleInputPassword1" class="col-form-label col-form-label-lg">Adress</label>
+
+									<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+
+								</div>
+
+								{/* //* Pincode */}
+								<div class="form-outline mb-4">
+									<label for="exampleInputPassword1" class="col-form-label col-form-label-lg">Pincode</label>
+
+									<input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password" />
+
+								</div>
+
+
+								<div class="form-check">
+									<input type="checkbox" class="form-check-input" id="exampleCheck1" />
+									<label class="form-check-label" for="exampleCheck1">Remember me</label>
+								</div>
+								<br />
+
+
+								<button
+									type="submit"
+									className="btn btn-lg btn-block"
+									disabled={
+										credentials.password.length <= 4 ||
+										!isEmailValid
+									}
+									style={{ backgroundColor: "#89b5f7" }}
+								>
+									Register
 								</button>
+								<br></br> <br></br>
+								<Anchor
+									para=""
+									details="Forgot password?"
+									link="#"
+									className="text-muted"
+								/>
 								<Anchor
 									para="Already have an account? "
 									details="Login here"
 									link="/user/login"
 									className="link-info"
 								/>
-							</div>
-						</form>
+
+
+
+							</form>
+
+						</div>
 					</div>
+
 				</div>
-			</div>
-		</div>
+			</section >
+
+		</>
+
+
+
 	);
 }
+
+export default UserRegister;
