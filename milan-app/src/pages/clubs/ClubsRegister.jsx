97db982e433434e150b4d65f72b32f882b03ffd9
Kushal <kushalghosh9899@gmail.com>
2022-02-11 01:38:48 +0530

    Added: Usestate for storing the data

--- milan-app/src/pages/clubs/ClubsRegister.jsx
+++ milan-app/src/pages/clubs/ClubsRegister.jsx
@@ -7,6 +7,16 @@ import "../../styles/ClubsRegister.css";
 const ClubLogin = () => {
     document.title = "Club Register | Milan";
 
+    const [credentials, setCredentials] = useState(
+        { 
+            name: "", 
+            email: "", 
+            password: "",
+            confirmPassword: "",
+            address: "",
+            pincode: "",
+         });
+
     return (
         <>
             <Navbar />
@@ -26,32 +36,38 @@ const ClubLogin = () => {
 
                             <div className="form-outline mb-2">
                                 <label className=" col-form-label col-form-label-lg">Club Name</label>
-                                <input type="email" className="form-control form-control-lg" placeholder="Enter your club name" />
+                                <input type="email" className="form-control form-control-lg" placeholder="Enter your club name" value={credentials.name}
+                                    onChange={(e) => setCredentials({...credentials, name: e.target.value })} />
                             </div>
 
                             <div className="form-outline mb-2">
                                 <label className=" col-form-label col-form-label-lg">Club Email</label>
-                                <input type="email" className="form-control form-control-lg" placeholder="Enter your email address" />
+                                <input type="email" className="form-control form-control-lg" placeholder="Enter your email address" value={credentials.email}
+                                    onChange={(e) => setCredentials({...credentials, email: e.target.value })} />
                             </div>
 
                             <div className="form-outline mb-2">
                                 <label className="col-form-label col-form-label-lg">Password</label>
-                                <input type="password" className="form-control form-control-lg" placeholder="Enter your password" />
+                                <input type="password" className="form-control form-control-lg" placeholder="Enter your password" value={credentials.password}
+                                    onChange={(e) => setCredentials({ ...credentials, password: e.target.value })} />
                             </div>
 
                             <div className="form-outline mb-2">
                                 <label className="col-form-label col-form-label-lg">Confirm your Password</label>
-                                <input type="password" className="form-control form-control-lg" placeholder="Confirm your password" />
+                                <input type="password" className="form-control form-control-lg" placeholder="Confirm your password" value={credentials.confirmPassword}
+                                    onChange={(e) => setCredentials({ ...credentials, confirmPassword: e.target.value })} />
                             </div>
 
                             <div className="form-outline mb-2">
                                 <label className="col-form-label col-form-label-lg">Address</label>
-                                <input type="text" className="form-control form-control-lg" placeholder="Enter your club address" />
+                                <input type="text" className="form-control form-control-lg" placeholder="Enter your club address" value={credentials.address}
+                                    onChange={(e) => setCredentials({ ...credentials, address: e.target.value })} />
                             </div>
 
                             <div className="form-outline mb-2">
                                 <label className="col-form-label col-form-label-lg">Pincode</label>
-                                <input type="text" maxLength="6" className="form-control form-control-lg" placeholder="Enter your pincode" />
+                                <input type="text" maxLength="6" className="form-control form-control-lg" placeholder="Enter your pincode" value={credentials.pincode}
+                                    onChange={(e) => setCredentials({ ...credentials, pincode: e.target.value })} />
                             </div>
 
                             <div className="d-grid gap-2 py-4">
