3d722d5bb6d2d31d136b2eda4b663196e38753d9
yeshwanth <m.yeshwanth235@gmail.com>
2022-02-10 22:01:57 +0530

    UserSchema is added in server/models

---
+++ server/models/UserSchema.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  pincode: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("user", UserSchema);
