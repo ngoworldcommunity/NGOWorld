1844742b1a4e121c5f136d39d393438034462c67
Harshit <harshitmittal070@gmail.com>
2022-02-12 14:05:36 +0530

    Return all users

--- server/routes/Display.js
+++ server/routes/Display.js
@@ -8,6 +8,14 @@ const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 
 //* Route 1  - Show all avaialble Users in the DB
+router.get("/allusers", async (req, res) => {
+  try {
+    const allusers = await User.find({});
+    return res.json(allusers);
+  } catch (error) {
+    return res.status(500);
+  }
+});
 
 //* Route 2 - Show all available Clubs in the DB
 
