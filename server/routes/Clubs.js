a97bb8998524f2affabb59fa8fc6d2bbc36b1af7
Kushal <kushalghosh9899@gmail.com>
2022-02-13 02:33:15 +0530

    Added: Backend for Club Registration

--- server/routes/Clubs.js
+++ server/routes/Clubs.js
@@ -7,5 +7,60 @@ const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
 
 //* Route 1  - Club Registration
+router.post("/register", async (req, res) => {
+    const {name,email,password,address,pincode,description} = req.body;
+
+    //Checks if the inputted field are empty or not
+    if(!email || !password || !name || !address || !pincode || !description){
+        return res.status(422).json({error:"Please add all the fields"});
+    }
+
+    Club.findOne({email:email})
+        .then((savedClub)=>{
+            //If already club is existing with same email
+            if(savedClub){
+                return res.json({ exists: true });
+            }
+            bcrypt.hash(password,12)
+            //hashing the password
+            .then(hashedpassword=>{                
+            const club = new Club({
+                email,
+                password:hashedpassword,
+                name,
+                address,
+                pincode,
+                description
+            })
+            club.save()//saving the data to db
+            .then(club=>{
+                const payload = {
+                    Club:{
+                        id: Club.id,
+                    },
+                };
+                jwt.sign(
+                    payload,
+                    "thisismysecret123",
+                    {
+                        expiresIn: 10000,
+                    },
+                    (error,token) =>{
+                        if(error)
+                            throw error;
+                        return res.json({ sucess: true })
+                    }
+                )              
+                
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+            })
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+})
 
 module.exports = router;
