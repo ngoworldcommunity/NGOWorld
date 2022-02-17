7b7427f6cceae645baae3e5f0bea6ae22078cbcb
mridul4101 <mridul.dgp2015@gmail.com>
2022-02-17 12:15:20 +0530

    Added backend route for club login

--- server/routes/Clubs.js
+++ server/routes/Clubs.js
@@ -8,46 +8,67 @@ var jwt = require("jsonwebtoken");
 
 //* Route 1  - Club Registration
 router.post("/register", async (req, res) => {
-  const { name, email, password, address, pincode, description } = req.body;
+    const { name, email, password, address, pincode, description } = req.body;
 
-  //Checks if the inputted field are empty or not
-  if (!email || !password || !name || !address || !pincode || !description) {
-    return res.status(422).json({ error: "Please add all the fields" });
-  }
+    //Checks if the inputted field are empty or not
+    if (!email || !password || !name || !address || !pincode || !description) {
+        return res.status(422).json({ error: "Please add all the fields" });
+    }
 
-  Club.findOne({ email: email })
-    .then((savedClub) => {
-      //If already club is existing with same email
-      if (savedClub) {
-        return res.json({ exists: true });
-      }
-      bcrypt
-        .hash(password, 12)
-        //hashing the password
-        .then((hashedpassword) => {
-          const club = new Club({
-            email,
-            password: hashedpassword,
-            name,
-            address,
-            pincode,
-            description,
-          });
-          club
-            .save() //saving the data to db
-            .then((club) => {
-              return res.json({ sucess: true });
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+    Club.findOne({ email: email })
+        .then((savedClub) => {
+            //If already club is existing with same email
+            if (savedClub) {
+                return res.json({ exists: true });
+            }
+            bcrypt
+                .hash(password, 12)
+                //hashing the password
+                .then((hashedpassword) => {
+                    const club = new Club({
+                        email,
+                        password: hashedpassword,
+                        name,
+                        address,
+                        pincode,
+                        description,
+                    });
+                    club
+                        .save() //saving the data to db
+                        .then((club) => {
+                            return res.json({ sucess: true });
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                });
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
 });
 
 //* Route 2 - Club Login
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const validEmail = await Club.findOne({ email });
+        const validPassword = await bcrypt.compare(password, validEmail.password);
+
+        const payload = { Club: { id: validEmail.email } };
+
+        if (validPassword) {
+            jwt.sign(payload, "HELLOSECRET123", (err, authToken) => {
+                return res.json({ status: true, authToken });
+            });
+        }
+        else {
+            return res.json({ status: false });
+        }
+    } catch (e) {
+        console.log(e);
+    }
+})
 
 module.exports = router;
