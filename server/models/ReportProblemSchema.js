3063040e4d2528c93a3659115281cfb9cda1a18e
Kushal <kushalghosh9899@gmail.com>
2022-02-20 02:01:25 +0530

    Fix: Added User Report route for backend
    Report a problem API route has been added

---
+++ server/models/ReportProblemSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const ReportProblemSchema = mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  reportmessage: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("report", ReportProblemSchema);
