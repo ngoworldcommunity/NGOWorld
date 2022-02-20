1145049d586bbb5a09895b8c0f462d98d734e7ea
Tamal Das <gyansujan69@gmail.com>
2022-02-20 10:22:59 +0530

    fix: Updated guidelines, removed extra codes.

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -10,6 +10,7 @@
 - Each issue needs to be completed within a specific time limit according to its difficulty level ( **`Easy`: 1 day, `Medium`: 2-3 days and `Hard`: 4 days )**.
 - If a mentee is unable to solve the allotted issue within the given period, the issue needs to be passed to other contenders on the discretion of the mentor.
 
+- You will be performing changes to **`tamal-milan`** branch ONLY.
 - `Fork` the repo to your account.
 - `git clone` to clone the repo
 
@@ -18,7 +19,11 @@
 - `git add .` to stage for commits
 - **NOTICE--> message will be `fix: <anything you fixed>`**
 - `git commit -am "fix: Updtated bla bla bla"` for commiting the code.
-- then create a Pull Request and wait for review.
+
+  <br/>
+
+- Then create a Pull Request and wait for review.
+- Pull request title : **`fix: bla bla bla`** , important to pass the checks.
 - Don't forget to attach **`Screenshots, Proper Description and Issue Number`**
 - Remember to be respectful to other contributors, mentors, comunnity
 - Don't raise invalid/spam issues they will be closed !!
@@ -29,4 +34,4 @@
 
 <br/>
 
-**You'll be needing to follow the (Setup Guidelines)[] for more details on setting up the code in your local machine**
+**You'll be needing to follow the [Setup Guidelines](https://github.com/IAmTamal/Milan/blob/main/rules/Setup.md) for more details on setting up the code in your local machine**
--- server/routes/Clubs.js
+++ server/routes/Clubs.js
@@ -8,68 +8,68 @@ var jwt = require("jsonwebtoken");
 
 //* Route 1  - Club Registration
 router.post("/register", async (req, res) => {
-    const { name, email, password, address, pincode, description } = req.body;
+  const { name, email, password, address, pincode, description } = req.body;
 
-    //Checks if the inputted field are empty or not
-    if (!email || !password || !name || !address || !pincode || !description) {
-        return res.status(422).json({ error: "Please add all the fields" });
-    }
+  //Checks if the inputted field are empty or not
+  if (!email || !password || !name || !address || !pincode || !description) {
+    return res.status(422).json({ error: "Please add all the fields" });
+  }
 
-    Club.findOne({ email: email })
-        .then((savedClub) => {
-            //If already club is existing with same email
-            if (savedClub) {
-                return res.json({ exists: true });
-            }
-            bcrypt
-                .hash(password, 12)
-                //hashing the password
-                .then((hashedpassword) => {
-                    const club = new Club({
-                        email,
-                        password: hashedpassword,
-                        name,
-                        address,
-                        pincode,
-                        description,
-                    });
-                    club
-                        .save() //saving the data to db
-                        .then((club) => {
-                            return res.json({ success: true });
-                        })
-                        .catch((err) => {
-                            console.log(err);
-                        });
-                });
-        })
-        .catch((err) => {
-            console.log(err);
+  Club.findOne({ email: email })
+    .then((savedClub) => {
+      //If already club is existing with same email
+      if (savedClub) {
+        return res.json({ exists: true });
+      }
+
+      //then we encrypt the password
+      bcrypt
+        .hash(password, 12)
+        //hashing the password
+        .then((hashedpassword) => {
+          const club = new Club({
+            email,
+            password: hashedpassword,
+            name,
+            address,
+            pincode,
+            description,
+          });
+          club
+            .save() //saving the data to db
+            .then((club) => {
+              return res.json({ success: true });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
         });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //* Route 2 - Club Login
 
 router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const validEmail = await Club.findOne({ email });
-        const validPassword = await bcrypt.compare(password, validEmail.password);
+  try {
+    const { email, password } = req.body;
+    const validEmail = await Club.findOne({ email });
+    const validPassword = await bcrypt.compare(password, validEmail.password);
 
-        const payload = { Club: { id: validEmail.email } };
+    const payload = { Club: { id: validEmail.email } };
 
-        if (validPassword) {
-            jwt.sign(payload, "HELLOSECRET123", (err, authToken) => {
-                return res.json({ success: true, authToken });
-            });
-        }
-        else {
-            return res.json({ success: false });
-        }
-    } catch (e) {
-        return res.json({ success: false });
-        console.log(e);
+    if (validPassword) {
+      jwt.sign(payload, "HELLOSECRET123", (err, authToken) => {
+        return res.json({ success: true, authToken, isuser: false });
+      });
+    } else {
+      return res.json({ success: false });
     }
-})
+  } catch (e) {
+    return res.json({ success: false });
+  }
+});
 
 module.exports = router;
