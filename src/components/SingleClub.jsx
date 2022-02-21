f63d1b751d2d7b74728bf7e6e35b11289f15bdc6
mridul4101 <mridul.dgp2015@gmail.com>
2022-02-21 14:04:21 +0530

    fix: updated props and styles acc to figma

--- src/components/SingleClub.jsx
+++ src/components/SingleClub.jsx
@@ -1,29 +1,48 @@
-import * as React from 'react';
-
+import * as React from "react";
 export default function SingleClub({ club }) {
+	const expand = () => {
+		document.getElementById(`less${club._id}`).classList.add("hidden");
+		document.getElementById(`more${club._id}`).classList.remove("hidden");
+	};
 
-    const handleReadMore = (e) => {
-        if (e.target.innerText === 'Read More...') {
-            document.getElementById(`desc-${club.id}`).classList.remove("cardReadLess");
-            document.getElementById(`desc-${club.id}`).classList.add("cardReadMore");
-            e.target.innerText = 'Read Less...';
-        } else {
-            document.getElementById(`desc-${club.id}`).classList.add("cardReadLess");
-            document.getElementById(`desc-${club.id}`).classList.remove("cardReadMore");
-            e.target.innerText = 'Read More...';
-        }
-    }
-    return (
-        <div className="card clubCard">
-            <img className="card-img-top" src={club.club_logo} alt={`${club.club_name} `} />
-            <div className="card-body">
-                <h5 className="card-title ">{club.club_name} </h5>
-                <p id={`desc-${club.id}`} className="card-text cardReadLess">{club.club_des}
-                </p>
-                <button className="btn btn-primary" onClick={handleReadMore}>Read More...</button>
+	const contract = () => {
+		document.getElementById(`more${club._id}`).classList.add("hidden");
+		document.getElementById(`less${club._id}`).classList.remove("hidden");
+	};
 
-            </div>
+	return (
+		<div className="card clubCard">
+			<img
+				className="card-img-top club-img"
+				src="https://i.ibb.co/0hxWDDD/1.jpg"
+				alt={`${club.name} `}
+			/>
+			<div className="card-body text-center">
+				<h3 className="card-title">{club.name} </h3>
+				<div className="club-info mt-3">
+					<p>{club.email}</p>
+					<p>{club.address}</p>
+				</div>
 
-        </div>
-    );
+				<div id={`less${club._id}`} className="desc">
+					{club.description.length > 80
+						? club.description.slice(0, 80) + "..."
+						: club.description}
+					{club.description.length > 80 ? (
+						<span className="seeBtn" onClick={expand}>
+							See more
+						</span>
+					) : (
+						""
+					)}
+				</div>
+				<div id={`more${club._id}`} className="hidden desc">
+					{club.description}
+					<span className="seeBtn" onClick={contract}>
+						See less
+					</span>
+				</div>
+			</div>
+		</div>
+	);
 }
