f71168f0c84de6a1dafb922a388322881590d9db
Tess-Vanta <72697074+Tess-Vanta@users.noreply.github.com>
2022-02-26 15:53:26 +0400

    feat: added new banner upon auth (#158)
    * fix: added new banner upon auth
    
    * fix: code improvement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import DonateBanner from "../components/DonateBanner";
 import HomeCardsContainer from "../components/HomeCardsContainer";
 import EventsBanner from "../components/Events";
+import LoginBanner from "../components/loginBanner";
 
+const AuthState = () => {
+  const [login, setLogin] = useState(localStorage.getItem("token"));
+  return login;
+};
 const Home = () => {
   return (
     <>
       <Navbar />
-      <Banner />
+      {AuthState() ? <LoginBanner /> : <Banner />}
       <HomeCardsContainer />
       <DonateBanner />
       <EventsBanner />
