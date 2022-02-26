f153b70e7e14799f17f69343ffe3b9f497b2e5fb
Tess-Vanta <tejaswimhtp@gmail.com>
2022-02-26 15:48:01 +0400

    fix: code improvement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
@@ -7,30 +7,21 @@ import HomeCardsContainer from "../components/HomeCardsContainer";
 import EventsBanner from "../components/Events";
 import LoginBanner from "../components/loginBanner";
 
+const AuthState = () => {
+  const [login, setLogin] = useState(localStorage.getItem("token"));
+  return login;
+};
 const Home = () => {
-  if (localStorage.getItem("token") === null) {
-    return (
-      <>
-        <Navbar />
-        <Banner />
-        <HomeCardsContainer />
-        <DonateBanner />
-        <EventsBanner />
-        <Footer />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Navbar />
-        <LoginBanner />
-        <HomeCardsContainer />
-        <DonateBanner />
-        <EventsBanner />
-        <Footer />
-      </>
-    );
-  }
+  return (
+    <>
+      <Navbar />
+      {AuthState() ? <LoginBanner /> : <Banner />}
+      <HomeCardsContainer />
+      <DonateBanner />
+      <EventsBanner />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
