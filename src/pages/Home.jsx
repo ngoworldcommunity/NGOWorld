1451625fb3aa1848518f7c8c7fa79944edebbdfa
Raj Roy <gyansujan69@gmail.com>
2022-02-27 09:49:09 +0530

    fixed caps

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 import DonateBanner from "../components/DonateBanner";
 import HomeCardsContainer from "../components/HomeCardsContainer";
 import EventsBanner from "../components/Events";
-import LoginBanner from "../components/LoginBanner";
+import LoginBanner from "../components/loginBanner";
 
 const AuthState = () => {
   const [login, setLogin] = useState(localStorage.getItem("token"));
