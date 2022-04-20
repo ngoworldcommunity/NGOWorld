633f394ee729cfed98c57e10b31dad2783cacc21
Tamal Das <gyansujan69@gmail.com>
2022-04-20 12:45:12 +0530

    fix : Events page styling

--- src/pages/display/EventsPage.jsx
+++ src/pages/display/EventsPage.jsx
@@ -4,6 +4,7 @@ import "../../styles/ClubsPage.css";
 import Navbar from "../../components/Navbar";
 import { GetAllClubs, GetAllEvents } from "../../service/MilanApi";
 import SingleEvent from "../../components/SingleEvent";
+import Eventspic from "../../assets/pictures/EventsPagefloating.svg"
 
 const EventsPage = () => {
 
@@ -23,6 +24,23 @@ const EventsPage = () => {
         <>
 
             <Navbar />
+            <div className="eventspageintro_parent">
+                <div className="eventspageintro_imgdiv">
+                    <img src={Eventspic} alt="events" />
+                </div>
+                <div className="eventspageintro_textdiv">
+                    <h2>Welcome to the events page</h2>
+                    <p>
+                        All our partnered NGOs , hosts various events be it
+                        educational, cleaning mother earth, funding events, health
+                        camps and many more !!
+                    </p>
+                    <p>Join us at the events, and help the community !! </p>
+                </div>
+            </div>
+
+            <hr className="container" />
+
             <div className="cards justify-content-center">
                 {clubData.map((club) => {
                     return <SingleEvent key={club._id} club={club} />;
