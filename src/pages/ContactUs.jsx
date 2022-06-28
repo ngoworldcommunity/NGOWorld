41f17c6d8ac3838dd4f7fbff2f5dff396becaa93
Hamza Ali <44437268+Hat52@users.noreply.github.com>
2022-06-28 08:45:39 +0500

    #191  Fix :  how the Contact Page looks  (#198)
    * fix:UI of contact page
    
    * fix: Contact page implemented according to the design

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,47 +7,52 @@ const ContactUs = () => {
 	return (
 		<>
 			<Navbar />
-			<div className="contactUs">
-				<div>
-					<img
-						src={contactImg}
-						alt="ContactUs"
-						className="contactUsImage"
-					/>
-				</div>
-				<div className="contactUsForm">
-					<h1 className="contactUsFormHeader">
-						Have something to say ? <br /> Contact us !!!
-					</h1>
-					<div className="contactUsFormNameInput">
-						<input
-							type="text"
-							placeholder="First Name"
-							className="contactUsFNFormInput"
-						/>
-						<input
-							type="text"
-							placeholder="Last Name"
-							className="contactUsLNFormInput"
-						/>
+			<div className="contact-us-container">
+				<div className="contact-us-form">
+					<h3>Have something to say ? <br /> Contact us !!!</h3>
+					<div className="text-field-div">
+						<div className="text-field-container">
+							<label htmlFor="first-name">Your First Name</label>
+							<input
+								type="text"
+								placeholder="First Name"
+								id="first-name"
+							/>
+						</div>
+						<div className="text-field-container">
+							<label htmlFor="last-name">Your Last Name</label>
+							<input
+								type="text"
+								placeholder="Last Name"
+								id="last-name"
+							/>
+						</div>
 					</div>
-					<div className="contactUsFormEmail">
-						<input
-							type="email"
-							placeholder="Enter your email"
-							className="contactUsEmailFormInput"
-						/>
+					<div className="text-field-div">
+						<div className="text-field-container">
+							<label htmlFor="email-address">Your Email Address</label>
+							<input
+								type="email"
+								placeholder="Email Address"
+								id="email-address"
+							/>
+						</div>
 					</div>
-					<div className="contactUsFormMsg">
-						<textarea
-							rows="6"
-							placeholder="Enter your message"
-							className="contactUsMsgFormInput"
-						/>
+					<div className="text-field-div">
+						<div className="text-field-container">
+							<label htmlFor="question">Enter you message</label>
+							<textarea 
+								id="question" 
+								name="question" 
+								rows="4" 
+								cols="50"
+								placeholder="Enter your message"
+							/>
+						</div>
 					</div>
-					<div className="contactUsFormSubmit">
-						<button className="contactUsSubmitBtn">Submit</button>
-					</div>
+					<button>Just Send
+					<svg style={{height: "20px", width: "20px", color: "rgb(255, 255, 255)", transform: "rotate(-40deg)"}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" fill="#fffff"></path> </svg>
+					</button>
 				</div>
 			</div>
 
