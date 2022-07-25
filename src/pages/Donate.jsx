f2420c6f4a4b7f7700d6d91f8bce2e8b0bca7170
Tamal Das <gyansujan69@gmail.com>
2022-07-25 11:42:16 +0530

    Changes made to the donations page (#240)
     - New Donations page
     - Working Modal for improved amount donations
     - Fixes with Responsiveness

---
+++ src/pages/Donate.jsx
@@ -0,0 +1,63 @@
+// This is the donate page where we come and select clubs to donate an amount !
+
+import React, { useEffect, useState } from 'react'
+import donate_image1 from "../assets/pictures/donate_image1.svg"
+import Navbar from "../components/Navbar"
+import SingleClub from '../components/SingleClub'
+import { GetAllClubs } from '../service/MilanApi'
+import "../styles/Donate.css"
+
+const Donate = () => {
+
+    const [clubData, setClubData] = useState([]);
+
+    useEffect(() => {
+        const fetchClubData = async () => {
+            const response = await GetAllClubs();
+            setClubData(response);
+        };
+        fetchClubData();
+    }, []);
+
+
+    return (
+        <>
+
+            <Navbar />
+
+            <div id="donate_banner"
+                className="container"
+            >
+
+                <div id="donateCol2">
+                    <img src={donate_image1} alt="" className="donate_img" />
+                </div>
+
+
+                <div
+                    id="donatecol_1"
+                    className="d-flex flex-column justify-content-center align-items-start me-5"
+                >
+                    <h2 className="mb-4">Yes, you help live !!</h2>
+                    <p>Donations does play an important part as our annual funds, donations from your ends helps thousands of unfortunate people live their lives. </p>{" "}
+
+                    <p>Choose any club, donate whatever you want, even 5 rupees helps !</p>
+
+
+                </div>
+
+            </div>
+
+
+            <hr className="container" />
+
+            <div className="cards justify-content-center">
+                {clubData.map((club) => {
+                    return <SingleClub key={club._id} club={club} />;
+                })}
+            </div>
+        </>
+    )
+}
+
+export default Donate
