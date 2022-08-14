06d8612bd323054221a56d8d45ac38e026754d88
RoeiY <roeiyaacobi@gmail.com>
2022-08-14 21:08:01 +0300

    add a banner for clubs

--- src/pages/display/ClubsPage.jsx
+++ src/pages/display/ClubsPage.jsx
@@ -1,30 +1,49 @@
 import React, { useEffect, useState } from "react";
 import SingleClub from "../../components/SingleClub";
 import "../../styles/ClubsPage.css";
+import clubs_banner from "../../assets/pictures/clubs-banner.svg";
 import Navbar from "../../components/Navbar";
 import { GetAllClubs } from "../../service/MilanApi";
 
 const ClubsPage = () => {
-	const [clubData, setClubData] = useState([]);
+  const [clubData, setClubData] = useState([]);
 
-	useEffect(() => {
-		const fetchClubData = async () => {
-			const response = await GetAllClubs();
-			setClubData(response);
-		};
-		fetchClubData();
-	}, []);
+  useEffect(() => {
+    const fetchClubData = async () => {
+      const response = await GetAllClubs();
+      setClubData(response);
+    };
+    fetchClubData();
+  }, []);
 
-	return (
-		<>
-			<Navbar />
-			<div className="cards justify-content-center">
-				{clubData.map((club) => {
-					return <SingleClub key={club._id} club={club} />;
-				})}
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div id="clubs_banner" className="container">
+        <div id="clubsCol2">
+          <img src={clubs_banner} alt="clubs banner" className="clubs_img" />
+        </div>
+
+        <div
+          id="clubscol_1"
+          className="d-flex flex-column justify-content-center align-items-start me-5"
+        >
+          <h2 className="mb-4">Clubs and communities !</h2>
+          <p>
+            Here are some clubs you can follow, you can attend club events and
+            even get notified about it once you subscribe !
+          </p>
+        </div>
+      </div>
+
+      <hr className="container" />
+      <div className="cards justify-content-center">
+        {clubData.map((club) => {
+          return <SingleClub key={club._id} club={club} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default ClubsPage;
