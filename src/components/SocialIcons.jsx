bcad7e736199006d319a305be1eb5c8ff559948b
Arpit Verma <arpitv970@gmail.com>
2022-10-04 14:27:19 +0530

    Increased sized of social icons

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const SocialIcons = () => {
   return (
     <ul className="social-icons list-inline mt-5">
-      <li className="list-inline-item">
+      <li className="list-inline-item mx-2">
         <a href="https://twitter.com/mrTamall" target="_blank" title="twitter">
-          <i className="fa fa-twitter" style={{color:"#9ac2fe"}}></i>
+          <i className="fa fa-2x fa-twitter" style={{color:"#9ac2fe"}}></i>
         </a>
       </li>
-      <li className="list-inline-item">
+      <li className="list-inline-item mx-2">
         <a href="https://github.com/IAmTamal/Milan" target="_blank" title="github">
-          <i className="fa fa-github"  style={{color:"#9ac2fe"}}></i>
+          <i className="fa fa-2x fa-github"  style={{color:"#9ac2fe"}}></i>
         </a>
       </li>
     </ul>
