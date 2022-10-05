55daa1ee42d492327d897c6dd91c0bed1b542d8a
Aazam Thakur <59562284+Alcadeus0@users.noreply.github.com>
2022-10-05 20:26:19 +0530

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import '../styles/Products.css';
-import Cards from "./Cards";
+import ProdcutsCard from "./Cards";
 
 const Products = () => {
     return (
         <div className="group">
             <h1>PRODUCTS</h1>
             <hr/>
-            <Cards />
+            <ProductsCard />
         </div>
     )
 }
