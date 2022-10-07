f6a05c1f215aa542b5d55128bea7584d27ab162d
nishtha53 <68805896+nishtha53@users.noreply.github.com>
2022-10-07 16:58:26 +0530

    fix: design issues of login page

--- src/pages/clubs/ClubsLogin.jsx
+++ src/pages/clubs/ClubsLogin.jsx
@@ -114,7 +114,7 @@ function ClubLogin() {
             <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
               <form style={{ width: "auto" }} onSubmit={handleSubmit}>
                 <h1 style={{ letterSpacing: "1px", marginBottom: "25px" }}>
-                  Log in with your Club
+                  Log in with your Club!
                 </h1>
                 <div className="form-outline mb-4">
                 <label
