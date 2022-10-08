b4f641305f156c07f5906ad17c2ce639df13060c
Rohan Hazari <rohanhazari2012@gmail.com>
2022-10-09 01:08:01 +0530

    Added back to top button

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -1,4 +1,4 @@
-import React,{ useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 import BannerBg from "../../assets/pictures/milanBg.jpg";
 import "../../styles/Banner.css";
