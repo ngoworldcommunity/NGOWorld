863fd5c537b1baf25ee1a3461de203b56921d738
Chandan Gupta <me.chandangupta3110@gmail.com>
2022-10-08 23:08:49 +0530

    Club and User Login Form UI Improvements, Better Transition (#421)
    * Login Page Remember Me Font Family Fix
    
    * UI Fixes in Club and User Login, Smoother Transition
    
    * UI Improvements (Requested Changes)
    
    * UI Improvements (Requested Changes)
    
    Co-authored-by: Tamal Das <gyansujan69@gmail.com>

--- src/pages/clubs/ClubsLogin.jsx
+++ src/pages/clubs/ClubsLogin.jsx
@@ -10,222 +10,239 @@ import "react-toastify/dist/ReactToastify.css";
 import Cookies from "js-cookie";
 
 function ClubLogin() {
-  document.title = "Milan | Club Login";
-  const Navigate = useNavigate();
+    document.title = "Milan | Club Login";
+    const Navigate = useNavigate();
 
-  function Anchor(props) {
-    return (
-      <div>
-        <p>
-          {props.para}
-          <Link to={props.link}>{props.details}</Link>
-        </p>
-      </div>
-    );
-  }
+    function Anchor(props) {
+        return (
+            <div>
+                <p>
+                    {props.para}
+                    <Link to={props.link}>{props.details}</Link>
+                </p>
+            </div>
+        );
+    }
 
-  const [credentials, setCredentials] = useState({
-    email: "",
-    password: "",
-  });
-
-  const [isEmailValid, setIsEmailValid] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleChange = (e) => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-    if (
-      e.target.name === "email" &&
-      e.target.value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
-    )
-      setIsEmailValid(true);
-  };
-
-  //* SUBMIT
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setIsLoading(true);
-    const Data = LoginClub(credentials);
-    setIsLoading(false);
-
-    Data.then((response) => {
-      if (response.data.success === true) {
-        Cookies.set("club", response.data.authToken);
-
-        toast("🌈 Logging you in !", {
-          position: "top-right",
-          autoClose: 1000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          closeButton:false,
-          onClose: () => {
-            Navigate("/");
-          },
-        });
-      } else if (response.data.success === false) {
-        toast("🌈 Error !", {
-          position: "top-right",
-          autoClose: 1000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          closeButton:false,
-        });
-        setCredentials({
-          email: "",
-          password: "",
-        });
-      }
-    }).catch((err) => {
-      console.log(err);
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: "",
     });
-  };
 
-  return (
-    <>
-      <Navbar />
+    const [isEmailValid, setIsEmailValid] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-      <ToastContainer
-        position="top-right"
-        autoClose={1000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        closeButton={false}
-      />
+    const handleChange = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+        if (
+            e.target.name === "email" &&
+            e.target.value.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")
+        )
+            setIsEmailValid(true);
+    };
 
-      <section className="vh-100">
-        <div className="container py-5 h-100">
-          <div className="row d-flex align-items-center justify-content-center h-100">
-            <div className="col-md-8 col-lg-7 col-xl-6 d-flex justify-content-center">
-              <img src={Pic} className="mobile-img" alt="profile-img"></img>
-            </div>
+    //* SUBMIT
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-            <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
-              <form style={{ width: "auto" }} onSubmit={handleSubmit} >
-                <h1 style={{ letterSpacing: "1px", marginBottom: "25px" }}>
-                  Log in with your Club!
-                </h1>
-                <div className="form-outline mb-4">
-                <label
-                  htmlFor="exampleInputEmail1"
-                  className="col-form-label col-form-label-lg regformlabels"
-                >
-                  Email address 📨
-                </label>
-                  <input
-                    type="email"
-                    className="desktop form-control form-control-lg color"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    placeholder="Enter your email"
-                    name="email"
-                    value={credentials.email}
-                    onChange={handleChange}
-                    required
-                  />
+        setIsLoading(true);
+        const Data = LoginClub(credentials);
+        setIsLoading(false);
 
-                  <input
-                    type="email"
-                    className="mobile form-control form-control-lg color"
-                    id="exampleInputEmail1"
-                    aria-describedby="emailHelp"
-                    name="email"
-                    placeholder="Enter your email"
-                    value={credentials.email}
-                    onChange={handleChange}
-                    required
+        Data.then((response) => {
+            if (response.data.success === true) {
+                Cookies.set("club", response.data.authToken);
 
-                  />
+                toast("🌈 Logging you in !", {
+                    position: "top-right",
+                    autoClose: 1000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    closeButton: false,
+                    onClose: () => {
+                        Navigate("/");
+                    },
+                });
+            } else if (response.data.success === false) {
+                toast("🌈 Error !", {
+                    position: "top-right",
+                    autoClose: 1000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    closeButton: false,
+                });
+                setCredentials({
+                    email: "",
+                    password: "",
+                });
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    };
+
+    return (
+        <>
+            <Navbar />
+
+            <ToastContainer
+                position="top-right"
+                autoClose={1000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                closeButton={false}
+            />
+
+            <section className="vh-100">
+                <div className="container py-5 h-100">
+                    <div className="row d-flex align-items-center justify-content-center h-100">
+                        <div className="col-md-8 col-lg-7 col-xl-6 d-flex justify-content-center">
+                            <img
+                                src={Pic}
+                                className="mobile-img"
+                                alt="profile-img"
+                            ></img>
+                        </div>
+
+                        <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+                            <form
+                                style={{ width: "auto" }}
+                                onSubmit={handleSubmit}
+                            >
+                                <h1
+                                    style={{
+                                        letterSpacing: "1px",
+                                        marginBottom: "25px",
+                                        textAlign: "center"
+                                    }}
+                                >
+                                    Log in with your Club!
+                                </h1>
+                                <div className="form-outline mb-4">
+                                    <label
+                                        htmlFor="exampleInputEmail1"
+                                        className="col-form-label col-form-label-lg regformlabels"
+                                    >
+                                        Email address 📨
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className="desktop form-control form-control-lg color"
+                                        id="exampleInputEmail1"
+                                        aria-describedby="emailHelp"
+                                        placeholder="Enter your email"
+                                        name="email"
+                                        value={credentials.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+
+                                    <input
+                                        type="email"
+                                        className="mobile form-control form-control-lg color"
+                                        id="exampleInputEmail1"
+                                        aria-describedby="emailHelp"
+                                        name="email"
+                                        placeholder="Enter your email"
+                                        value={credentials.email}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+
+                                <div className="form-outline mb-4">
+                                    <label
+                                        htmlFor="exampleInputPassword1"
+                                        className="col-form-label col-form-label-lg regformlabels color"
+                                    >
+                                        Password 🔑
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className="desktop form-control form-control-lg color"
+                                        id="exampleInputPassword1"
+                                        placeholder="Enter your password"
+                                        name="password"
+                                        value={credentials.password}
+                                        onChange={handleChange}
+                                        required
+                                    />
+
+                                    <input
+                                        type="password"
+                                        className="mobile form-control form-control-lg color"
+                                        id="exampleInputPassword1"
+                                        name="password"
+                                        value={credentials.password}
+                                        placeholder="Enter your password"
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+
+                                {/* RememberMe Tab  */}
+                                <div id="rememberMe" className="form-check color-change">
+                                    <input
+                                        type="checkbox"
+                                        className="form-check-input color-change-input"
+                                        id="exampleCheck1"
+                                    />
+                                    <label id="remember-me" className="form-check-label" htmlFor="exampleCheck1">
+                                        Remember me
+                                    </label>
+                                </div>
+                                <br />
+                                <div className="btn-container">
+                                    <button
+                                        disabled={credentials.password.length <= 4 || !isEmailValid}
+                                        type="submit"
+                                        className="login-btn btn btn-lg btn-block"
+                                        style={{ backgroundColor: "#C996CC" }}
+                                    >
+                                        {isLoading ? (
+                                            <TailSpin color="#FFFFFF" height={30} width={30} />
+                                        ) : (
+                                            "LOGIN"
+                                        )}
+                                    </button>
+
+
+                                </div>
+                                <br></br>
+                                <br></br>
+                                <div className="anchor-container">
+                                    <Anchor
+                                        para=""
+                                        details="Forgot password?"
+                                        link="/clubs/forgotpass"
+                                        className="text-muted"
+                                    />
+                                    <Anchor
+                                        para="Don't have an account? "
+                                        details="Register here"
+                                        link="/clubs/register"
+                                        className="link-info"
+                                    />
+
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="form-outline mb-4">
-                <label
-                    htmlFor="exampleInputPassword1"
-                    className="col-form-label col-form-label-lg regformlabels color"
-                  >
-                    Password 🔑
-                  </label>
-                  <input
-                    type="password"
-                    className="desktop form-control form-control-lg color"
-                    id="exampleInputPassword1"
-                    placeholder="Enter your password"
-                    name="password"
-                    value={credentials.password}
-                    onChange={handleChange}
-                    required
-                  />
-                  
-                  <input
-                    type="password"
-                    className="mobile form-control form-control-lg color"
-                    id="exampleInputPassword1"
-                    name="password"
-                    value={credentials.password}
-                    placeholder="Enter your password"
-                    onChange={handleChange}
-                    required
-                  />
-                </div>
-
-                {/* RememberMe Tab  */}
-                <div id="rememberMe" className="form-check color-change">
-                  <input
-                    type="checkbox"
-                    className="form-check-input color-change-input"
-                    id="exampleCheck1"
-                  />
-                  <label className="form-check-label" htmlFor="exampleCheck1">
-                    Remember me
-                  </label>
-                </div>
-                <br />
-
-                <button
-                  disabled={credentials.password.length <= 4 || !isEmailValid}
-                  type="submit"
-                  className="login-btn btn btn-lg btn-block"
-                  style={{ backgroundColor: "#C996CC" }}
-                >
-                  {isLoading ? (
-                    <TailSpin color="#FFFFFF" height={30} width={30} />
-                  ) : (
-                    "LOGIN"
-                  )}
-                </button>
-
-                <br></br>
-                <br></br>
-                <Anchor
-                  para=""
-                  details="Forgot password?"
-                  link="/clubs/forgotpass"
-                  className="text-muted"
-                />
-                <Anchor
-                  para="Don't have an account? "
-                  details="Register here"
-                  link="/clubs/register"
-                  className="link-info"
-                />
-              </form>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+            </section>
+        </>
+    );
 }
 
 export default ClubLogin;
