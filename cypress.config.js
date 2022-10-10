c39ab3100eda171b32f2db2502a5d768552d7f18
Sau Juliana <julienyange@gmail.com>
2022-10-10 12:03:15 +0300

    fix: github action trial 17

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "**/*.spec.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
