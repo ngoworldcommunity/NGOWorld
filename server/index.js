ecf4263cb96e303f227f35823be57170e891ee98
Tinesh Nehete <74779225+Tineshnehete@users.noreply.github.com>
2022-10-25 02:49:28 +0530

    Fix for new events not created (#520)
    * Fixing .env use after accessing env vars
    
    * Fixing issue "events not created"

--- server/index.js
+++ server/index.js
@@ -3,13 +3,13 @@ const express = require("express");
 var cors = require("cors");
 const dotenv = require("dotenv");
 
+dotenv.config();
 connectToMongo();
 const app = express();
 let port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
 
 app.get("/", (req, res) => {
   res.send("HELLO FROM HOME");
