ba6f7523ee2afbcad00c73cf023085d03f51a1b5
Subhrajit Dhar <108873732+devwithsubhrajit2002@users.noreply.github.com>
2022-10-31 22:02:54 +0530

    modified the import files

--- src/pages/clubs/EventCreate.jsx
+++ src/pages/clubs/EventCreate.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
 import '../../styles/EventCreate.css';
 import Events from '../../assets/pictures/CreateEventsPic.svg';
