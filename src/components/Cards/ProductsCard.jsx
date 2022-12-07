8470a3229f436f6ac8336dc041e4e25f00dd130c
Haris <harisshah385@gmail.com>
2022-12-07 22:25:02 +0500

    revert changes

--- src/components/Cards/ProductsCard.jsx
+++ src/components/Cards/ProductsCard.jsx
@@ -9,46 +9,38 @@ const ProductsCard = () => {
     <div className="card-group">
       <div className="cardart">
         <img src={face} className="art" alt="art" />
-        <div className="card-content">
-          <div>
-            <img src={star} className="star" alt="start" />
-          </div>
-          <h6>Art: A girl's dream</h6>
-          <h6>$20</h6>
+        <div>
+          <img src={star} className="star" alt="start" />
         </div>
+        <h6>Art: A girl's dream</h6>
+        <h6>$20</h6>
       </div>
 
       <div className="cardart">
         <img src={art} className="art" alt="art" />
-        <div className="card-content">
-          <div>
-            <img src={star} className="star" alt="star" />
-          </div>
-          <h6>Art: Celebrate Inside</h6>
-          <h6>$20</h6>
+        <div>
+          <img src={star} className="star" alt="star" />
         </div>
+        <h6>Art: Celebrate Inside</h6>
+        <h6>$20</h6>
       </div>
 
       <div className="cardart">
         <img src={face} className="art" alt="art" />
-        <div className="card-content">
-          <div>
-            <img src={star} className="star" alt="star" />
-          </div>
-          <h6>Art: A girl's dream</h6>
-          <h6>$20</h6>
+        <div>
+          <img src={star} className="star" alt="star" />
         </div>
+        <h6>Art: A girl's dream</h6>
+        <h6>$20</h6>
       </div>
 
       <div className="cardart">
         <img src={art} className="art" alt="art" />
-        <div className="card-content">
-          <div>
-            <img src={star} className="star" alt="star" />
-          </div>
-          <h6>Art: Celebrate Inside</h6>
-          <h6>$20</h6>
+        <div>
+          <img src={star} className="star" alt="star" />
         </div>
+        <h6>Art: Celebrate Inside</h6>
+        <h6>$20</h6>
       </div>
     </div>
   );
