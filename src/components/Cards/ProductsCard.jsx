3683b8d0a18999e363301c0cc63f23e2e1b83429
aadarsh805 <aadarshthakur80501@gmail.com>
2022-12-09 15:08:06 +0530

    added carousel in products section in mobile screen size

--- src/components/Cards/ProductsCard.jsx
+++ src/components/Cards/ProductsCard.jsx
@@ -3,44 +3,74 @@ import star from "../../assets/pictures/star.png";
 import face from "../../assets/pictures/girlface.svg";
 import art from "../../assets/pictures/artpiece.svg";
 import "../../styles/ProductsCard.css";
+import useEmblaCarousel from "embla-carousel-react";
 
-const ProductsCard = () => {
+const ProductsCard = ({
+  options = {
+    align: "center",
+    breakpoints: {
+      "(min-width: 500px)": { active: false },
+      slidesToScroll: 2,
+      skipSnaps: false,
+    },
+  },
+}) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
   return (
-    <div className="card-group">
-      <div className="cardart">
-        <img src={face} className="art" alt="art" />
-        <div>
-          <img src={star} className="star" alt="start" />
+    <div ref={emblaRef}>
+      <div className="card-group w-full">
+        <div className="cardart ">
+          <img src={face} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="start" />
+          </div>
+          <h6>Art: A girl's dream</h6>
+          <h6>$20</h6>
         </div>
-        <h6>Art: A girl's dream</h6>
-        <h6>$20</h6>
-      </div>
 
-      <div className="cardart">
-        <img src={art} className="art" alt="art" />
-        <div>
-          <img src={star} className="star" alt="star" />
+        <div className="cardart">
+          <img src={art} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="star" />
+          </div>
+          <h6>Art: Celebrate Inside</h6>
+          <h6>$20</h6>
         </div>
-        <h6>Art: Celebrate Inside</h6>
-        <h6>$20</h6>
-      </div>
+        <div className="cardart">
+          <img src={art} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="star" />
+          </div>
+          <h6>Art: Celebrate Inside</h6>
+          <h6>$20</h6>
+        </div>
+        <div className="cardart">
+          <img src={art} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="star" />
+          </div>
+          <h6>Art: Celebrate Inside</h6>
+          <h6>$20</h6>
+        </div>
 
-      <div className="cardart">
-        <img src={face} className="art" alt="art" />
-        <div>
-          <img src={star} className="star" alt="star" />
+        <div className="cardart">
+          <img src={face} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="star" />
+          </div>
+          <h6>Art: A girl's dream</h6>
+          <h6>$20</h6>
         </div>
-        <h6>Art: A girl's dream</h6>
-        <h6>$20</h6>
-      </div>
 
-      <div className="cardart">
-        <img src={art} className="art" alt="art" />
-        <div>
-          <img src={star} className="star" alt="star" />
+        <div className="cardart">
+          <img src={art} className="art" alt="art" />
+          <div>
+            <img src={star} className="star" alt="star" />
+          </div>
+          <h6>Art: Celebrate Inside</h6>
+          <h6>$20</h6>
         </div>
-        <h6>Art: Celebrate Inside</h6>
-        <h6>$20</h6>
       </div>
     </div>
   );
