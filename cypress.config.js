258ecc41aecbb34a10691e394570b6397d69e7ee
Tamal <gyansujan69@gmail.com>
2023-01-03 12:09:28 +0530

    fix : Changes made to dev settings

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173/',
+    baseUrl: 'http://localhost:3000/',
     specPattern: "**/*.spec.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
