5274b336419fca069956658780019f044bfa1e27
Satyam Negi <sn.satyam2014@gmail.com>
2023-02-17 00:36:19 +0530

    fix: made detailed club page responsive

--- src/pages/clubs/ClubDetails.jsx
+++ src/pages/clubs/ClubDetails.jsx
@@ -11,33 +11,31 @@ function ClubDetailsCard() {
   const getclubdetails = async () => {
     const data = await getClubDetails(params.id);
     setclubdetails(data);
-  }
+  };
 
   useEffect(() => {
-    getclubdetails()
-
-  }, []);
+    getclubdetails();
+  }, [clubdetails]);
 
   return (
-    <div className="club_details">
-      <div className="club_details_img">
-        <img src={club_details_img} />
-      </div>
-      <div className="club_details_card">
-        <div>
-          <h1>Welcome to {clubdetails?.name}</h1>
-          <h2> {clubdetails?.tagLine} </h2>
-          <h6>
-            <span>📌</span> {clubdetails?.address}
-          </h6>
-          <p>
-            {clubdetails?.description}
-          </p>
-
-          <button>Contact Us</button>
+    <section>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 col-lg-6 col-12 m-auto pt-3 club_details_img">
+            <img src={club_details_img} />
+          </div>
+          <div className="col-sm-12 col-md-12 col-lg-6 col-12 club_details_card">
+            <h1>Welcome to {clubdetails?.name}</h1>
+            <h2>{clubdetails?.tagLine} </h2>
+            <h6>
+              <span>📌</span> {clubdetails?.address}
+            </h6>
+            <p>{clubdetails?.description}</p>
+            <button>Contact Us</button>
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
