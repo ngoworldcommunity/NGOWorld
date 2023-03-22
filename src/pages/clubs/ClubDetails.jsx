b38f98bed9748fb6b2210dea20ed6a5e8096bc80
Satyam Negi <sn.satyam2014@gmail.com>
2023-03-22 16:35:28 +0530

    Made the requested changes in club details page

--- src/pages/clubs/ClubDetails.jsx
+++ src/pages/clubs/ClubDetails.jsx
@@ -25,10 +25,13 @@ function ClubDetailsCard() {
             <img src={club_details_img} />
           </div>
           <div className="col-sm-12 col-md-12 col-lg-6 col-12 club_details_card">
-            <h1>Welcome to {clubdetails?.name}</h1>
+            <h1>
+              <span className="welcome_text">Welcome to</span>
+              {clubdetails?.name}
+            </h1>
             <h2>{clubdetails?.tagLine} </h2>
             <h6>
-              <span>📌</span> {clubdetails?.address}
+              <span>📍</span> {clubdetails?.address}
             </h6>
             <p>{clubdetails?.description}</p>
             <button>Contact Us</button>
