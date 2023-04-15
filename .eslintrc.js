a935a0110399e65d2ffcce29dfc7bc12d7d746de
Tamal Das <gyansujan69@gmail.com>
2023-04-15 11:27:28 +0530

    feat: added linters & husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 4],
+    "linebreak-style": ["error", "unix"],
+    "jsx-quotes": ["error", "prefer-double"],
+    semi: ["error", "always"],
+    "no-multiple-empty-lines": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  },
 };
