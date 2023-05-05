cf8f18ed3f1960333e55ecd654e42236f43ddb4b
Tamal Das <tamalcodes@gmail.com>
2023-05-06 01:00:58 +0530

    feat: adding razorpay payment gateway

--- src/pages/clubs/ClubDetails.jsx
+++ src/pages/clubs/ClubDetails.jsx
@@ -1,44 +1,119 @@
-import React, { useEffect, useState } from "react";
-import club_details_img from "../../assets/pictures/clubpage-group-discussion.svg";
+import React, { useEffect, useRef } from "react";
 import "../../styles/ClubDetails.css";
 import { useParams } from "react-router-dom";
-import { getClubDetails } from "../../service/MilanApi";
+import displayRazorpay from "../../service/PaymentGateway";
+import Modal from "../../components/Modal";
+import useSWR from "swr";
+import { defaultfetcher } from "../../utils/fetcher";
 
 function ClubDetailsCard() {
   const params = useParams();
-  const [clubdetails, setclubdetails] = useState();
+  const inputRef = useRef(null);
+  const [showPaymodal, setshowPaymodal] = React.useState(false);
 
-  const getclubdetails = async () => {
-    const data = await getClubDetails(params.id);
-    setclubdetails(data);
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => {
+        resolve(true);
+      };
+      script.onerror = () => {
+        resolve(false);
+      };
+      document.body.appendChild(script);
+    });
   };
 
   useEffect(() => {
-    getclubdetails();
-  }, [clubdetails]);
+    loadScript("https://checkout.razorpay.com/v1/checkout.js");
+  });
+
+  const { data: clubdetails } = useSWR(
+    `${import.meta.env.VITE_MILANAPI}/display/clubs?id=${params.id}`,
+    defaultfetcher,
+  );
+
+  const closePayModal = () => {
+    setshowPaymodal(false);
+    document.body.style.overflow = "auto";
+  };
 
   return (
-    <section>
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-12 col-md-12 col-lg-6 col-12 m-auto pt-3 club_details_img">
-            <img src={club_details_img} />
-          </div>
-          <div className="col-sm-12 col-md-12 col-lg-6 col-12 club_details_card">
-            <h1>
-              <span className="welcome_text">Welcome to</span>
-              {clubdetails?.name}
-            </h1>
-            <h2>{clubdetails?.tagLine} </h2>
-            <h6>
-              <span>📍</span> {clubdetails?.address}
-            </h6>
+    <>
+      <div className="clubdetails_parent">
+        <div className="clubdetails_imagediv">
+          <img src="https://i.ibb.co/88dTvtR/We-love-earth.png" alt="" />
+        </div>
+
+        <div className="clubdetails_textdiv">
+          <h1>{clubdetails?.name}</h1>
+          <h2>{clubdetails?.tagLine}</h2>
+
+          <div className="clubdetails_description flexed">
+            <p>
+              {" "}
+              <span>Find us at : </span> {clubdetails?.address}{" "}
+            </p>
             <p>{clubdetails?.description}</p>
-            <button>Contact Us</button>
+
+            <button
+              className="btn"
+              onClick={() => {
+                setshowPaymodal(true);
+                document.body.style.overflow = "hidden";
+              }}
+            >
+              Help us grow
+            </button>
           </div>
         </div>
       </div>
-    </section>
+
+      <div className="clubdetails_description  clubdetails_description_landscape">
+        <p>
+          {" "}
+          <span>Find us at : </span> {clubdetails?.address}{" "}
+        </p>
+        <p>{clubdetails?.description}</p>
+
+        <button className="btn">Help us grow</button>
+      </div>
+
+      {showPaymodal && (
+        <Modal onClose={closePayModal}>
+          <div className="paymodal">
+            <h1 style={{ marginTop: 0 }}>Every donations matter</h1>
+            <p>
+              We are a non-profit organization and we don&apos;t take any profit
+              from the donations.
+            </p>
+
+            <div className="input-group ">
+              <span className="input-group-text"> ₹ </span>
+              <input
+                type="number"
+                className="form-control"
+                aria-label="Amount (to the nearest rupee)"
+                placeholder="Enter an amount"
+                ref={inputRef}
+              />
+            </div>
+
+            <button
+              className="btn"
+              onClick={() => {
+                const money = inputRef.current.value;
+                if (money === null) return;
+                displayRazorpay(money);
+              }}
+            >
+              Pay now
+            </button>
+          </div>
+        </Modal>
+      )}
+    </>
   );
 }
 
