9a8ca04b574436b5f9369e1c023c93e78ca3a2ae
Himanshu Rawat <97152850+him-raw99@users.noreply.github.com>
2023-05-23 17:26:49 +0530

    add error 404 page

---
+++ src/pages/Error404.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Error404() {
+  return (
+    <>
+        <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates earum voluptate quod corrupti iste aspernatur eveniet esse nihil. Temporibus dignissimos eligendi iure sed quae? Distinctio omnis dolorem sequi rem quae veniam! Ratione, nobis.
+        </h1>
+    </>
+  )
+}
+
+export default Error404
