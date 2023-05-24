3db33a46ee037dc1aac25330f81506a45d92454a
Himanshu Rawat <97152850+him-raw99@users.noreply.github.com>
2023-05-24 23:49:33 +0530

    reduce the size of image + improve ui

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import error404Svg from "../assets/pictures/error404.svg";
 import Button from "../components/Button";
-import "../styles/Error404.css"
+import "../styles/Error404.css";
 function Error404() {
   return (
     <>
       <img src={error404Svg} alt="error-404" className="error-img" />
       <div className="button-wrapper home-btn">
-      <Button to="/" children={"Go Back To Homepage"} />
+      <Button to="/" children={"Homepage"} />
       </div>
     </>
   );
