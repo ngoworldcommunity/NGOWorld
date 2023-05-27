2319727e7b2a4eea67b2e06bdf85674101dfd914
Nitish Kumar Jha <106099275+Nitish-hack@users.noreply.github.com>
2023-05-27 07:35:02 -0700

    feat(products): add API to add products (#745)
    Co-authored-by: Tamal Das <tamalcodes@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()]
-})
+  plugins: [svgr(), react()],
+});
