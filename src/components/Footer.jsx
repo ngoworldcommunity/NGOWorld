de5beccb4716c128c84958a1dcb96609ec6a5152
roopesh <vetcharoopesh@gmail.com>
2023-05-28 05:48:30 +0530

    issue#761 updated the "here" more and removed the style component as it is added in the footer.css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -202,7 +202,7 @@ const Footer = () => {
                   href="https://github.com/IAmTamal/Milan"
                   target="_blank"
                   rel="noreferrer"
-                  style={{ color: "#e26959" }}
+                className="underline-animation"
                 >
                   here
                 </a>
