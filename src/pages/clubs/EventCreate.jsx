8b210f1fe4f2658f3eb5e6e8c7e9925f5406cd38
Suzy <sulagnaghosh52@gmail.com>
2023-06-08 22:37:33 +0530

    feat: add autocomplete places api and componenent

--- src/pages/clubs/EventCreate.jsx
+++ src/pages/clubs/EventCreate.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import Navbar from "../../components/Navbar";
 import "../../styles/EventCreate.css";
 import Events from "../../assets/pictures/CreateEventsPic.svg";
 import { CreateEvent } from "../../service/MilanApi";
-
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  GeoapifyGeocoderAutocomplete,
+  GeoapifyContext,
+} from "@geoapify/react-geocoder-autocomplete";
+import "@geoapify/geocoder-autocomplete/styles/minimal.css";
 
 export default function EventCreate() {
   const [eventdetails, seteventdetails] = useState({
@@ -47,6 +52,11 @@ export default function EventCreate() {
     }
   };
 
+  const handleEventLocation = (e) => {
+    eventdetails.eventlocation = JSON.stringify({ data: e });
+    seteventdetails({ ...eventdetails });
+  };
+
   return (
     <>
       <ToastContainer
@@ -62,22 +72,29 @@ export default function EventCreate() {
         closeButton={false}
       />
 
-      <section className="vh-100">
+      <section>
         <div className="container py-5 h-100">
-          <div className="row d-flex align-items-center justify-content-center h-100">
+          <div className="row d-flex align-items-center justify-content-center ">
             <div className="col-md-8 col-lg-7 col-xl-6">
-              <img src={Events} width="90%" alt="profile-img"></img>
+              <img src={Events} width="90%" alt="profile-img" />
             </div>
 
             <div className="col-md-7 col-lg-5 col-xl-5 offset-xl-1">
+              <h1 className="eventCreateWelcome  mr-10 mb-15">
+                Create event for your club
+              </h1>
               <form style={{ width: "auto" }}>
-                <h1 className="eventCreateWelcome">
-                  Create an event for your club
-                </h1>
-                <div className="form-outline">
+                <div className="form-outline mb-4">
+                  <label
+                    htmlFor="name"
+                    className="col-form-label col-form-label-lg"
+                    style={{ fontFamily: "Open Sans, sans-serif" }}
+                  >
+                    What's your event called?
+                  </label>
                   <input
                     type="text"
-                    className="eventCreateFormInput form-control "
+                    className="form-control form-control-lg remove_placeholder_desktop"
                     id="eventName"
                     name="eventname"
                     required
@@ -86,10 +103,17 @@ export default function EventCreate() {
                     onChange={handleChange}
                   />
                 </div>
-                <div className="form-outline">
+                <div className="form-outline mb-4">
+                  <label
+                    htmlFor="date"
+                    className="col-form-label col-form-label-lg"
+                    style={{ fontFamily: "Open Sans, sans-serif" }}
+                  >
+                    Date of the event?
+                  </label>
                   <input
-                    type="text"
-                    className="eventCreateFormInput form-control form-control-md"
+                    type="date"
+                    className="form-control form-control-lg remove_placeholder_desktop"
                     id="eventdate"
                     name="eventdate"
                     required
@@ -98,10 +122,17 @@ export default function EventCreate() {
                     onChange={handleChange}
                   />
                 </div>
-                <div className="form-outline">
+                <div className="form-outline mb-4">
+                  <label
+                    htmlFor="email"
+                    className="col-form-label col-form-label-lg"
+                    style={{ fontFamily: "Open Sans, sans-serif" }}
+                  >
+                    Time of the event?
+                  </label>
                   <input
-                    type="text"
-                    className="eventCreateFormInput form-control form-control-md"
+                    type="time"
+                    className="form-control form-control-lg remove_placeholder_desktop"
                     id="eventtime"
                     name="eventtime"
                     required
@@ -110,22 +141,36 @@ export default function EventCreate() {
                     onChange={handleChange}
                   />
                 </div>
-                <div className="form-outline">
-                  <input
-                    type="text"
-                    className="eventCreateFormInput form-control form-control-md"
-                    id="eventlocation"
-                    name="eventlocation"
-                    required
-                    placeholder="Where will the event take place?"
-                    value={eventdetails.eventlocation}
-                    onChange={handleChange}
-                  />
+                <div className="form-outline mb-4">
+                  <label
+                    htmlFor="email"
+                    className="col-form-label col-form-label-lg"
+                    style={{ fontFamily: "Open Sans, sans-serif" }}
+                  >
+                    Where will the event take place?
+                  </label>
+
+                  <GeoapifyContext
+                    apiKey={`${import.meta.env.VITE_GEOAPIFY_API_KEY}`}
+                  >
+                    <GeoapifyGeocoderAutocomplete
+                      name="eventlocation"
+                      id="form-control form-control-lg remove_placeholder_desktop"
+                      placeSelect={(e) => handleEventLocation(e)}
+                    />
+                  </GeoapifyContext>
                 </div>
-                <div className="form-outline">
+                <div className="form-outline mb-4">
+                  <label
+                    htmlFor="email"
+                    className="col-form-label col-form-label-lg"
+                    style={{ fontFamily: "Open Sans, sans-serif" }}
+                  >
+                    Tell us something more about the event
+                  </label>
                   <textarea
                     type="text"
-                    className="eventCreateFormInput form-control form-control-lg"
+                    className="form-control form-control-lg remove_placeholder_desktop"
                     id="eventTime"
                     name="eventdescription"
                     placeholder="Tell us something more about the event"
@@ -135,7 +180,7 @@ export default function EventCreate() {
                 </div>
                 <button
                   type="button"
-                  className="btn  eventCreateSubmit"
+                  className="btn btn-lg eventCreateSubmit"
                   onClick={(e) => {
                     handleClick(e);
                   }}
