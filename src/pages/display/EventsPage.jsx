dc18f25f0ad29cf570c69ef7e02b8d2f0d61b5a2
Sulagna Ghosh <62666287+suzy-g38@users.noreply.github.com>
2023-07-21 19:50:52 +0530

    feat: search clubs and events based on location and places (#852)
    Co-authored-by: Tamal Das <tamalcodes@gmail.com>

--- src/pages/display/EventsPage.jsx
+++ src/pages/display/EventsPage.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import Loading from "../../components/Loading";
 import SingleClubEvent from "../../components/SingleClubEvent";
 import useSWR from "swr";
 import { defaultfetcher } from "../../utils/fetcher";
+import { filter } from "../../utils/filter";
+import states from "./StatesData";
+import Button from "../../components/Button";
+import { showErrorToast } from "../../utils/showToast";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 
@@ -12,6 +16,95 @@ const EventsPage = () => {
     `${import.meta.env.VITE_MILANAPI}/display/allevents`,
     defaultfetcher,
   );
+
+  const [filterState, setFilterState] = useState({
+    chosenFilter: "",
+    showFilter: false,
+    chosenData: {},
+    searchLoading: false,
+  });
+
+  const { chosenFilter, showFilter, chosenData, searchLoading } = filterState;
+
+  const handleStateEvents = (state) => {
+    setFilterState((prevState) => ({
+      ...prevState,
+      chosenData: prevState.chosenData.data === state ? {} : { data: state },
+      chosenFilter: "place",
+    }));
+  };
+
+  const handleChooseFilter = (type) => {
+    if (type === "place") {
+      setFilterState((prevState) => ({
+        ...prevState,
+        chosenFilter: "",
+        showFilter: !prevState.showFilter,
+      }));
+    }
+    if (type === "location") {
+      if (chosenFilter === "location") {
+        setFilterState((prevState) => ({
+          ...prevState,
+          chosenFilter: "",
+          showFilter: false,
+        }));
+      } else {
+        getPositionFilter();
+        setFilterState((prevState) => ({
+          ...prevState,
+          chosenFilter: "location",
+          showFilter: false,
+        }));
+      }
+    }
+  };
+
+  const getPositionFilter = async () => {
+    try {
+      setFilterState((prevState) => ({
+        ...prevState,
+        searchLoading: true,
+      }));
+
+      const permissionStatus = await navigator.permissions.query({
+        name: "geolocation",
+      });
+
+      if (permissionStatus.state !== "granted") {
+        setFilterState((prevState) => ({
+          ...prevState,
+          chosenData: {},
+          searchLoading: true,
+        }));
+      }
+
+      const position = await getPosition();
+
+      setFilterState((prevState) => ({
+        ...prevState,
+        chosenData: {
+          data: {
+            lat: position?.coords?.latitude,
+            lon: position.coords.longitude,
+          },
+        },
+      }));
+    } catch (error) {
+      showErrorToast("Error getting location");
+    } finally {
+      setFilterState((prevState) => ({
+        ...prevState,
+        searchLoading: false,
+      }));
+    }
+  };
+
+  const getPosition = () => {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -32,13 +125,57 @@ const EventsPage = () => {
         <div className="cp_main_parent">
           <div className="cp_subparent">
             <div className="cp_textdiv">
-              <p className="cp_header1">Events happening now !</p>
+              <p className="cp_header1">Events happening now!</p>
               <p className="cp_header2">
-                All our partnered NGOs , hosts various events be it educational,
-                cleaning mother earth, funding events, health camps and many
-                more !
+                All our partnered NGOs host various events, be it educational,
+                cleaning mother earth, funding events, health camps, and many
+                more!
               </p>
             </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="outlined"
+              className="mx-2 nav_signin_btn"
+              style={{
+                background: chosenFilter === "location" ? "#e26959" : "",
+              }}
+              onClick={() => handleChooseFilter("location")}
+            >
+              <h4>Find Events near You</h4>
+            </Button>
+            <Button
+              variant="outlined"
+              className="mx-2 nav_signin_btn"
+              style={{ background: showFilter ? "#e26959" : "" }}
+              onClick={() => handleChooseFilter("place")}
+            >
+              <h4>Find Events by States</h4>
+            </Button>
+          </div>
+          <div className="filter-option">
+            {showFilter &&
+              !isLoading &&
+              states.map((state, index) => (
+                <Button
+                  variant="outlined"
+                  className="nav_signin_btn"
+                  style={{
+                    background:
+                      chosenData && chosenData.data === state ? "#e26959" : "",
+                  }}
+                  key={index}
+                  onClick={() => handleStateEvents(state)}
+                >
+                  {state}
+                </Button>
+              ))}
           </div>
 
           <div className="cp_cardsdiv">
@@ -46,15 +183,27 @@ const EventsPage = () => {
               <Loading />
             ) : (
               <>
-                {eventsData?.map((event) => {
-                  return (
-                    <SingleClubEvent
-                      key={event?._id}
-                      event={event}
-                      type="events"
-                    />
-                  );
-                })}
+                {searchLoading && <Loading />}
+                {!searchLoading &&
+                  (() => {
+                    const filteredEvents = filter(
+                      eventsData,
+                      chosenFilter,
+                      chosenData,
+                      "Eventlocation",
+                    );
+                    if (filteredEvents.length === 0) {
+                      return <p className="cp_header2">No Events Found</p>;
+                    } else {
+                      return filteredEvents.map((event) => (
+                        <SingleClubEvent
+                          key={event?._id}
+                          event={event}
+                          type="events"
+                        />
+                      ));
+                    }
+                  })()}
               </>
             )}
           </div>
