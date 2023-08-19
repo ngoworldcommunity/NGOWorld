edb41c4aff0e09b7796a82a420310a100028c4c1
yashpamecha <yj69771@gmail.com>
2023-08-19 14:46:08 +0530

    chore(text): update text

--- src/utils/PasswordValidation.js
+++ src/utils/PasswordValidation.js
@@ -12,21 +12,27 @@ export default function validatePassword(props) {
   } else if (!props.password.match(lowerCase)) {
     return {
       auth: false,
-      message: "Password should contains lowercase letters!",
+      message: "Password should contain at least one lowercase alphabet",
     };
   } else if (!props.password.match(upperCase)) {
     return {
       auth: false,
-      message: "Password should contains uppercase letters!",
+      message: "Password should contain at least one uppercase alphabet",
     };
   } else if (!props.password.match(numbers)) {
-    return { auth: false, message: "Password should contains numbers also!" };
+    return {
+      auth: false,
+      message: "Password should contain at least one number",
+    };
   } else if (props.password.length < 8) {
-    return { auth: false, message: "Password should not be less than 8" };
+    return {
+      auth: false,
+      message: "Password should not be less than 8 characters",
+    };
   } else if (!props.password.match(special)) {
     return {
       auth: false,
-      message: "Password should contains atleast one special characters",
+      message: "Password should contains atleast one special character",
     };
   } else
     return {
