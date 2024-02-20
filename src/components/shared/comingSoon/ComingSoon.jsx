c263af3b546f564bcff79ef3267acbba1fb131a5
Nayan Desai <ndmaratha01@gmail.com>
2024-02-20 12:14:08 +0530

    fix: correct signup notification for logged-in users
    Currently, logged-in users are incorrectly shown a signup notification
    when accessing certain routes, such as /events and /shop, on the website.
    
    To address this issue, this commit modifies the relevant components to
    utilize local storage for accessing the isLoggedIn status. Now, when a
    user is already logged in, the notification displayed on these routes
    appropriately reflects their authenticated status.
    
    Changes Made:
    - Updated relevant components to check the isLoggedIn status from local
      storage and adjust the notification accordingly.
    - Added conditional rendering logic to show different notifications based
      on the user's authentication status.
    - Updated tests to ensure the correct behavior of the modified components.

--- src/components/shared/comingSoon/ComingSoon.jsx
+++ src/components/shared/comingSoon/ComingSoon.jsx
@@ -4,16 +4,14 @@ import Button from "../buttons/globalbutton/Button";
 import "./ComingSoon.css";
 
 const ComingSoon = ({ launchitem }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   useEffect(() => {
     // Check if the user object exists in localStorage
     const userJSON = localStorage.getItem("persist:root");
-
     if (userJSON) {
       // Parse the JSON string to get the JavaScript object
       const { user } = JSON.parse(userJSON);
       const { isLoggedIn } = JSON.parse(user);
-
       // Access the isLoggedIn property and set the state
       setIsLoggedIn(isLoggedIn);
     }
