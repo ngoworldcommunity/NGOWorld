0190f1ff35d9009b1b9a39ab0e8d48c2fa942687
Nayan Desai <ndmaratha01@gmail.com>
2024-02-21 22:57:46 +0530

    fix: hide 'signup' button displayed on events and shop pages for logged-in users #1222
    feat: added you will be notified div when user is logged in
    
    style: added style for above div in comingsoon.css

--- src/components/shared/comingSoon/ComingSoon.jsx
+++ src/components/shared/comingSoon/ComingSoon.jsx
@@ -1,21 +1,10 @@
-import { useEffect, useState } from "react";
 import ComingSoonLogo from "../../../assets/pictures/comingsoon.svg";
 import Button from "../buttons/globalbutton/Button";
 import "./ComingSoon.css";
+import { useSelector } from "react-redux";
 
 const ComingSoon = ({ launchitem }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect(() => {
-    // Check if the user object exists in localStorage
-    const userJSON = localStorage.getItem("persist:root");
-    if (userJSON) {
-      // Parse the JSON string to get the JavaScript object
-      const { user } = JSON.parse(userJSON);
-      const { isLoggedIn } = JSON.parse(user);
-      // Access the isLoggedIn property and set the state
-      setIsLoggedIn(isLoggedIn);
-    }
-  }, []);
+  const isLoggedIn = useSelector((store) => store.user.isLoggedIn);
   return (
     <>
       <div className="comingsoon_parent">
@@ -25,6 +14,7 @@ const ComingSoon = ({ launchitem }) => {
           We will let you know whenever we launch our{" "}
           {launchitem ? launchitem : "page"}.
         </p>
+
         {isLoggedIn ? (
           <div className="notification-div">You will be Notified</div>
         ) : (
