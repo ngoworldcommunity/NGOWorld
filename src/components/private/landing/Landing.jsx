c7e50f49c6ee07655beb4dca640673fe845d4251
Piyush-Linux <piyuhpatil98@protonmail.com>
2024-02-27 23:24:06 +0530

    feat: floating header

--- src/components/private/landing/Landing.jsx
+++ src/components/private/landing/Landing.jsx
@@ -60,6 +60,24 @@ const Landing = () => {
             {windowWidth > 430
               ? "We are OpenSource, you can contribute too!"
               : "We are OpenSource, contribute now !"}{" "}
+            <button>
+              <span>Check it Out</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M13.5 10.5L21 3m-5 0h5v5m0 6v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5"
+                />
+              </svg>
+            </button>
           </span>
         </div>
 
